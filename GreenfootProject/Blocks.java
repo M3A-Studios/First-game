70f2802bed9bccfe8fd7227905b65291f7c023fc
Daaku <alexanderborgers@outlook.com>
2019-10-05 19:41:41 +0200

    Whatever this should be fine now

--- GreenfootProject/Blocks.java
+++ GreenfootProject/Blocks.java
@@ -4,7 +4,7 @@ public class Blocks extends Actor
 {  
     Blocks(int ID) {
         GreenfootImage image = new GreenfootImage (ID + ".png");
-        image.scale((image.getWidth() / Options.scalingFactor),(image.getHeight() / Options.scalingFactor));
+        image.scale((Options.widthSize),(Options.heightSize));
         setImage(image);
     }
 }
